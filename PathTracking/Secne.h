8a41f5548496eb0609c14e8c9e57adb4787ce4d0
xiong5517018432 <xiong5517018432@163.com>
2021-11-29 20:24:14 +0800

    test

--- PathTracking/Secne.h
+++ PathTracking/Secne.h
@@ -2,8 +2,8 @@
 #include "common.h"
 class Scene {
 public:
-	int  WIDTH = 720;
-	int HEIGHT = 720;
+	int  WIDTH =720;
+	int HEIGHT =720;
 	int  SCREEN_Z = 1;// 投影平面的Z坐标
 
 	vector<Shape*> shapes;  // 几何物体的集合
